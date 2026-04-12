b1257067d4cae6a511d9259e74eff5d5eb6ebd1f
whoishuang00 <2994366975@qq.com>
2026-04-12 21:52:17 +0800

    会话功能-spring整合langchain4j

---
+++ consultant/src/main/java/com/gdxy/consultant/controller/ChatController.java
@@ -0,0 +1,20 @@
+package com.gdxy.consultant.controller;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+    /*
+    * langchain4j-open-ai-spring-boot-starter这个起步依赖会自动注入OpenAiChatModel
+    * */
+    @Autowired
+    private OpenAiChatModel model;
+    @RequestMapping("/chat")
+    public String chat(String message){
+        String result=model.chat(message);
+        return result;
+    }
+}
