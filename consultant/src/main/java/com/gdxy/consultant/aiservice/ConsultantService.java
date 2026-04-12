e1bf8528b4662bbab42a4cbf128411b898cf178c
whoishuang00 <2994366975@qq.com>
2026-04-12 22:44:38 +0800

    会话功能-流式调用

--- consultant/src/main/java/com/gdxy/consultant/aiservice/ConsultantService.java
+++ consultant/src/main/java/com/gdxy/consultant/aiservice/ConsultantService.java
@@ -2,15 +2,19 @@ package com.gdxy.consultant.aiservice;
 
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
+import reactor.core.publisher.Flux;
 
 //方法二
 @AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配
-        chatModel = "openAiChatModel"//指定模型
+        chatModel = "openAiChatModel",//指定模型
+        streamingChatModel = "openAiStreamingChatModel"
 )
 //    方法三 自动装配
 //@AiService
 public interface ConsultantService {
 //    用于聊天的方法
-    public String chat(String message);
+//    public String chat(String message);
+
+    public Flux<String> chat(String message);
 }
