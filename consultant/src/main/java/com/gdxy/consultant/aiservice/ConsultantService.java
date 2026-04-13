bd8757875dac50ed0e940ee9bcbd11adf9bc95e1
whoishuang00 <2994366975@qq.com>
2026-04-13 11:06:05 +0800

    会话功能-消息注解

--- consultant/src/main/java/com/gdxy/consultant/aiservice/ConsultantService.java
+++ consultant/src/main/java/com/gdxy/consultant/aiservice/ConsultantService.java
@@ -1,5 +1,8 @@
 package com.gdxy.consultant.aiservice;
 
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
@@ -15,6 +18,16 @@ import reactor.core.publisher.Flux;
 public interface ConsultantService {
 //    用于聊天的方法
 //    public String chat(String message);
+//    @SystemMessage("你是黄谨来的小助理，人美心善又多金")
+//    public Flux<String> chat(String message);
 
-    public Flux<String> chat(String message);
+//    @SystemMessage(fromResource = "system.txt")
+//    public Flux<String> chat(String message);
+
+//    @UserMessage("你是黄谨来的小助理，人美心善又多金!{{it}}")
+//    public Flux<String> chat(String message);
+
+    @UserMessage("你是黄谨来的小助理，人美心善又多金!{{msg}}")
+    public Flux<String> chat(@V("msg") String message);
+
 }
